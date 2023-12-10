f2710de713d5432aa539bc6df0eb0c231db765ec
azeTrom17 <nmort17@aol.com>
2023-12-09 23:21:03 -0600

    improved hud

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -4,5 +4,58 @@ using UnityEngine;
 
 public class HUD : MonoBehaviour
 {
+    public void Exit()
+    {
+        Application.Quit();
+    }
 
+    public void Save()
+    {
+
+    }
+
+    public void LoadSaveFile(int saveFile)
+    {
+
+    }
+
+    public void PlayStop()
+    {
+
+    }
+
+    public void PauseResume()
+    {
+
+    }
+
+    public void Pulser()
+    {
+
+    }
+
+    public void Magnet()
+    {
+
+    }
+
+    public void Node()
+    {
+
+    }
+
+    public void NodeColor(int color)
+    {
+
+    }
+
+    public void Erase()
+    {
+
+    }
+
+    public void Tutorial()
+    {
+
+    }
 }
